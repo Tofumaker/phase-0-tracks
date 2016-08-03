fe14b1bd2ec797a7df894ec5220ec2c42e49bad6
Tofumaker <dyy2105@columbia.edu>
2016-08-02 20:27:54 -0400

    Clean up formatting and aesthetics

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -1,26 +1,26 @@
 puts("Name of hamster?")
 hname = gets.chomp
 
-puts("volume level? (1-10)")
+puts("Volume level? (1-10)")
 hvolume = gets.chomp
 
-puts("fur color?")
+puts("Fur color?")
 hcolor = gets.chomp
 
-puts("good for adoption? (y/n)")
+puts("Good for adoption? (y/n)")
 hadoption = gets.chomp
 
-puts("estimated age?")
+puts("Estimated age?")
 hage = gets.chomp 
 
 if hage ==""
   hage=nil
-  puts("You didn't enter anything!")
+  puts("You didn't enter age!")
 else 
 puts("age:"+hage)
 end  
 
-puts("name:"+hname)
-puts("vol:"+hvolume) 
-puts("color"+hcolor)
-puts("Good?"+hadoption) 
+puts("name: "+hname)
+puts("vol: "+hvolume) 
+puts("color: "+hcolor)
+puts("Good?: "+hadoption) 
