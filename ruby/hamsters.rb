5c47dd387e61a034f2312a3741cfabbd36cbd542
jaymep <jaymep@users.noreply.github.com>
2016-08-02 17:49:36 -0700

    Add variable update, data type info
    Release 2 & 3 - update variables to correct data type & correct print statements accordingly

--- ruby/hamsters.rb
+++ ruby/hamsters.rb
@@ -3,24 +3,33 @@ hname = gets.chomp
 
 puts("Volume level? (1-10)")
 hvolume = gets.chomp
+hvolume = hvolume.to_i
 
 puts("Fur color?")
 hcolor = gets.chomp
 
-puts("Good for adoption? (y/n)")
+puts("Good for adoption? (Y/N)")
 hadoption = gets.chomp
+if hadoption == 'Y'
+	hadoption = TRUE
+elsif hadoption == 'N'
+	hadoption = FALSE
+else
+	hadoption = nil
+end
 
-puts("Estimated age?")
-hage = gets.chomp  
+puts("Estimated age (yrs)? (Ex: 0.5)")
+hage = gets.chomp
+hage = hage.to_f
 
 puts("Name: "+hname)
-puts("Vol: "+hvolume) 
+puts("Vol: "+hvolume.to_s) 
 puts("Color: "+hcolor)
-puts("Good?: "+hadoption) 
+puts("Good?: "+hadoption.to_s) 
 
 if hage ==""
   hage=nil
   puts("You didn't enter age!")
 else 
-  puts("Age: "+hage)
+  puts("Age: "+hage.to_s)
 end 
