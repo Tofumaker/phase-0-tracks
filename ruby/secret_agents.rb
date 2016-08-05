dbf516f53d3cfda97d9498cae6399b9480bc471a
Tofumaker <dyy2105@columbia.edu>
2016-08-04 21:31:37 -0400

    provide insightful comments

--- ruby/secret_agents.rb
+++ ruby/secret_agents.rb
@@ -5,10 +5,13 @@
 # => Otherwise
 # =>   Advance letter to next letter in alphabet
 
+# Method declarations:
 
-
+# encrypt method, encrypts the password
 def encrypt(password)
+  #e_pw is intended to store encrypted password string
   e_pw = password
+  #i is for iteration
   i = password.length
   while i > 0
     if e_pw[i-1] == "z"
@@ -28,9 +31,11 @@ end
 #   get index in alphabet
 #   add index plus one to decrypted password
 
+#decrypt method, decrypts the password
 def decrypt(encr)
   a_key = "abcdefghijklmnopqrstuvwxyz"
   d_pw = encr
+  #j is for iteration
   j = encr.length-1
   for i in 0..j
     current_letter = a_key.index(encr[i].to_s)
@@ -46,11 +51,25 @@ end
 # decrypt("afe")
 
 #The output of the first method is a string, and these methods can be used in any string.
-decrypt(encrypt("swordfish"))
+# decrypt(encrypt("swordfish"))
 
 #INTERFACE
+# Ask user whether they'd like to encrypt or decrypt
+# While user gives the right answer
+#   if answer is encrypt
+#     ask for input 
+#     call encrypt method
+#   if answer is decrypt
+#     ask for input 
+#     call decrypt method
+#   Otherwise, 
+#     prompt user again for encrypt or decrypt
+# Finished!
+
+# Driver Code
 
 user_error = 1
+
 while user_error > 0
   puts("Would you like to encrypt or decrypt (e/d)?")
   choice = gets.chomp
