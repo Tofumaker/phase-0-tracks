1e6e9472b729e9fcaa5af3de48f100973aae31f7
Tofumaker <dyy2105@columbia.edu>
2016-08-14 17:46:47 -0400

    create basic hash of movies with a few keys in various formats

---
+++ ruby/nested_data_structures.rb
@@ -0,0 +1,21 @@
+movies = {
+  favorite:{
+    name: "Minority Report"
+    run_time: 145
+    cast: ["Tom Cruise","Colin Farrell", "Samantha Morton"]
+    family_friendly: false
+  }
+  second_favorite:{
+    name: "Collateral"
+    run_time: 97
+    cast: ["Jamie Foxx", "Tom Cruise", "Jada Pinkett Smith"]
+    family_friendly: false
+  }
+  third_favorite:{
+    name: "Star Wars"
+    run_time: 135
+    cast: ["Mark Hamil", "Harrison Ford","Carrie Fisher", "James Earl Jones"]
+    family_friendly: true
+  }
+}
+
