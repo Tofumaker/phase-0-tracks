271aff6ebda752760e27b327d612e0801e5c9194
Tofumaker <dyy2105@columbia.edu>
2016-08-15 00:37:40 -0400

    Complete vowel and non vowel methods, implemented them, nest all within loop to repeat

--- ruby/alias_manager.rb
+++ ruby/alias_manager.rb
@@ -11,13 +11,72 @@
   #store the alias in the second position of the Key's array
   #prompt user to repeat
 
+#DATA STORAGE*****
+#Spies hash will house keys named 'agent 0', 'agent 1', etc...
+#each key will contain an array that houses the real name and alias in positions 0 and 1 respectively
+
 spies = {}
-n=0 #for iteration of entire process
+n=0 #for iteration of agent key numbers
 repeat = true
+
+#define vowels and non vowels
+$vowels = "AaEeIiOoUu"
+$not_vowels = "BbCcDdFfGgHhJjKkLlMmNnPpQqRrSsTtVvWwXxYyZz"
+
+#Methods for changing both vowels and non-vowels
+def vowel_change(current_v)
+  if current_v == "u"
+    current_v = "a"
+  elsif current_v == "U"
+    current_v = "A"  
+  else  
+    v_location = $vowels.index(current_v)
+    current_v = $vowels[v_location+2]
+  end
+  current_v
+end    
+
+def not_vowel_change(current_nv)
+  if current_nv == "z"
+    current_nv = "b"
+  elsif current_nv == "Z"
+    current_nv = "B"
+  else  
+    nv_location = $not_vowels.index(current_nv)
+    current_nv = $not_vowels[nv_location+2]
+  end
+  current_nv
+end    
+
+
+#BEGINNING OF MAIN WHILE LOOP
 while repeat == true
-  p "Enter full name"
-  spies["agent "+n.to_s] = gets.chomp
-  p spies
+  #Prompt user for name and then implement previously defined methods
+  current_spy=[]
+  p "Enter full name of Agent "+n.to_s
+  full_name = gets.chomp
+  current_spy[0] = full_name
+
+  #swap first name and last name
+  temp_array = full_name.split(' ')
+  temp_array.reverse!
+  temp_string = temp_array.join(' ')
+
+  l = temp_string.length - 1
+  while l >= 0
+    if $vowels.include?(temp_string[l])
+      temp_string[l] = vowel_change(temp_string[l])
+    elsif $not_vowels.include?(temp_string[l])
+      temp_string[l] = not_vowel_change(temp_string[l])
+    else
+    end  
+    l-=1  
+  end  
+  current_spy[1] = temp_string
+  spies["Agent "+n.to_s] = current_spy
+  p "Assigned ALIAS: " + temp_string
+
+  #repeat the entire process?
   p "Enter another name? (y/n)"
   user_choice = gets.chomp
   if user_choice == "y"||user_choice=="Y"
@@ -26,6 +85,14 @@ while repeat == true
   else
     repeat = false
   end
-end    
+end #END OF MAIN WHILE LOOP 
+
+#Print out the entire spy database
+p " "
+p "Just to repeat the information..."
+spies.each do |spy_number, dossier|
+  p spy_number + " is " + dossier[0] + ", AKA " + dossier[1]
+end  
+
         
 
