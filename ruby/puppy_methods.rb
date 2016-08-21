9ab29be737b1be9ea1ca749a6c691a2507a0f922
Tofumaker <dyy2105@columbia.edu>
2016-08-20 20:55:12 -0400

    Complete last release, store all chefs in an array and use methods

--- ruby/puppy_methods.rb
+++ ruby/puppy_methods.rb
@@ -44,3 +44,56 @@ Tofu.dog_years
 Tofu.speak(3)
 Tofu.roll_over
 Tofu.fetch("tennis ball")
+
+
+
+class Chef
+
+  def initialize
+    puts ("Training chef")
+  end
+
+  def pizza (toppings)
+    # puts("What would like on your Pizza?")
+    # toppings = gets.chomp
+    pizza_array = toppings.split(" ")
+    puts("Great, your pizza will have:")
+
+    pizza_array.each do |topping| 
+      puts topping
+    end    
+  end
+
+  def combo(order)
+    if order == 1
+      puts "Burger, fries, and a Coke coming up!"
+    elsif order == 2
+      puts "Chicken sandwich, chips, and Sprite comin up!"
+    elsif order == 3
+      puts "Salad and water...."
+    else
+      puts "Sorry that's not on the menu"
+    end
+  end          
+end    
+
+Wolfgang = Chef.new
+Wolfgang.pizza("pepperoni cheese garlic")
+
+# puts("What combo would you like?")
+# order = gets.chomp.to_i
+Wolfgang.combo(2)
+
+chef_array = []
+chef_number = 0
+while chef_number < 50
+  chef_array[chef_number] = Chef.new
+  chef_number += 1
+end
+
+chef_array.each do |cook|
+  puts ("Get ready for the best meal of your life!")
+  cook.pizza("cheese sausage onion")
+  cook.combo(2)
+end
+
