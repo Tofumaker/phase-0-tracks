b1e2b8e3019753a92a18e66648ee71c3ec020fb4
Tofumaker <dyy2105@columbia.edu>
2016-08-21 23:10:47 -0400

    create pseudocode for knight class

---
+++ ruby/solo.rb
@@ -0,0 +1,12 @@
+#Pseudocode
+#Design a 'Knight' class 
+#----------Attributes----------
+#Name: String
+#Health: Integer
+#Stamina: Integer
+#Evil: Boolean
+#----------Methods-------------
+#Swing Sword
+#Shoot Arrow
+#Drink Blood of Innocent
+
