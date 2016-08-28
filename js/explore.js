ac9f3c5793a00088e99d95ac78aeaef24dc32e1b
Tofumaker <dyy2105@columbia.edu>
2016-08-28 16:55:38 -0400

    Finish string reverse method by setting the for loop to correct parameters

--- js/explore.js
+++ js/explore.js
@@ -4,8 +4,11 @@
 // - Add each character to a temporary variable
 //Iterate through the entire string
 function reverse(str) {
-  for (var i=str.length; i>=0; i--){
-    var r_str = r_str+str[i]
+  var r_str = ""
+  for (var i=str.length-1; i>=0; i--){
+    r_str += str[i]
   }
   console.log(r_str)
 }
+
+reverse("Hello")
