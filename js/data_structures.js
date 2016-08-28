aa07c46b2b24f5ec057e898ca963a4ead145828c
Tofumaker <dyy2105@columbia.edu>
2016-08-28 19:22:32 -0400

    finish car constructor function and define a rev engine function.

--- js/data_structures.js
+++ js/data_structures.js
@@ -13,3 +13,27 @@ for (var i = colors.length-1; i >= 0; i--) {
 
 console.log(new_horses);
 
+
+function Car(make, seats, american){
+  this.make = make;
+  this.seats = seats;
+  this.american = american;
+
+  this.rev_engine = function(){
+    if (american==true){
+      console.log("VROOOOOOOM!");
+    }
+    else{
+      console.log("vroom");
+    }
+  }
+console.log(this)  
+this.rev_engine()
+}
+
+var new_car = new Car('Ford',5,true);
+// console.log(new_car);
+// new_car.rev_engine();
+
+var another_car = new Car('Toyota',2,false);
+// another_car.rev_engine();
