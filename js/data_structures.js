262a04def2e86afe697739bb880209c21fe11daa
Tofumaker <dyy2105@columbia.edu>
2016-08-28 18:54:23 -0400

    Add a for loop that will create horses with the names as keys and the colors as values of those keys

--- js/data_structures.js
+++ js/data_structures.js
@@ -1,7 +1,15 @@
-var colors = ["blue", "red", "green", "yellow"]
-var names = ["Secretariat", "Spirit", "Ed", "Hidalgo"]
-colors.push("magenta")
-names.push("Socks")
+var colors = ["blue", "red", "green", "yellow"];
+var names = ["Secretariat", "Spirit", "Ed", "Hidalgo"];
+colors.push("magenta");
+names.push("Socks");
 
-console.log(colors)
-console.log(names)
+// console.log(colors)
+// console.log(names)
+
+var new_horses ={};
+for (var i = colors.length-1; i >= 0; i--) {
+  new_horses[names[i]]=colors[i];
+}
+
+console.log(new_horses);
+
