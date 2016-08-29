6f50415efa72d20b9f911d10a973736cbbe60e91
Tofumaker <dyy2105@columbia.edu>
2016-08-29 02:56:54 -0400

    Create matching pair detector function

--- js/algos.js
+++ js/algos.js
@@ -7,6 +7,17 @@
 //output the storage variable
 //output any additional phrases if applicable
 
+//Pseudo Code for Matching Pair Finder
+//Create a function that takes two objects as input
+//Create a variable called matching as a boolean and set to false upon initialization 
+//For every key-and-value pair in first object, cycle through and compare every key-and-value pair in the second object
+//If both match, then set variable matching to true
+//Return the match variable at upon conclusion of function
+
+//
+
+//Pseudo Code for 
+
 //LONGEST STRING ALGORITHM
 function Longest(storage_array){
   var l_str = ""
@@ -30,9 +41,31 @@ function Longest(storage_array){
   }  
 }//END OF LONGEST STRING ALGORITHM
 
-
+function Matching(object_1, object_2){
+  var matched_pair = false
+  for (var key_1 in object_1) {
+    for (var key_2 in object_2) {
+      if (key_1 == key_2 && object_1[key_1] == object_2[key_2])
+        matched_pair = true
+    }
+  }
+  console.log("Matching Pair Detected: "+matched_pair)
+  return matched_pair
+}
 
 
 //DRIVER CODE TO TEST FUNCTIONS
 Longest(["This is a test","This is a test array","This is the last item"])
 Longest(["This is a test", "A second test", "Third and final here","Forth and final here"])
+
+Matching( {make:"Ford",year:1990,american:true} , {make:"toyota",year:2000,american:false})
+Matching( {make:"Ford",year:2000,american:true} , {make:"toyota",year:2000,american:false})
+Matching( {Make:"toyota",yearo:2000,americano:true} , {make:"toyota",year:2000,american:true})
+
+
+
+
+
+
+
+
