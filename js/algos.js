62f11e666f7643e92b4ad9128b9500f8b17331dd
Tofumaker <dyy2105@columbia.edu>
2016-08-28 21:45:40 -0400

    Correct code so that it takes an array for input.

--- js/algos.js
+++ js/algos.js
@@ -1,34 +1,38 @@
 //Pseudo Code for Longest Phrase Finder
-//Take a string input
-//Split it into separate words and store them in an array
+//Take an array of strings for input
 //Define a storage variable and keep it empty for now
 //Cycle through the array
 //If array item is longer than the storage variable, then set the storage variable equal to current array item.
-//After cycling through entire array, output the storage variable
+//After cycling through entire array, cycle through again to catch strings of the same length
+//output the storage variable
+//output any additional phrases if applicable
 
-function Longest(str){
+//LONGEST STRING ALGORITHM
+function Longest(storage_array){
   var l_str = ""
-  var other_long_words = []
-  var storage_array = str.split(" ")
-  //The for loop below finds the first instance of the longest word(s) in the given string
+  var other_long_phrases = []
+  //The for loop below finds the first instance of the longest phrase(s) in the given string
   for (var i = 0; i <= storage_array.length - 1; i++) {
     if (storage_array[i].length > l_str.length){
       l_str = storage_array[i]
       var str_index = i
     }
   }
-  //The for loop below loops through once more to find any other words of the same length
+  //The for loop below loops through once more to find any other strings of the same length
   for (var i = 0; i <= storage_array.length -1; i++) {
     if (storage_array[i].length == l_str.length && str_index!=i){
-      other_long_words.push(storage_array[i])
+      other_long_phrases.push(storage_array[i])
     }
   }
-  console.log("The longest word is "+l_str)
-  if (other_long_words.length>0){
-    console.log("Other words of equal length are: "+ other_long_words)
-  }
-  
-}
+  console.log("The longest string is '"+l_str+"'")
+  if (other_long_phrases.length>0){
+    console.log("Other strings of equal length are: "+ other_long_phrases)
+  }  
+}//END OF LONGEST STRING ALGORITHM
 
-Longest("This is a test sentence")
-Longest("Here is a second test to make sure that the algorithm works. teeeeeest")
+
+
+
+//DRIVER CODE TO TEST FUNCTIONS
+Longest(["This is a test","This is a test array","This is the last item"])
+Longest(["This is a test", "A second test", "Third and final here","Forth and final here"])
